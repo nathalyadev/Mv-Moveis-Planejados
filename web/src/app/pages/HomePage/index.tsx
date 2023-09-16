eb9e7ead4271962829298d15aba4cec49065be32
nathalyadev <ns.contato21@gmail.com>
2023-09-16 13:58:57 -0300

    feat: Estrutura main

--- web/src/app/pages/HomePage/index.tsx
+++ web/src/app/pages/HomePage/index.tsx
@@ -1,9 +1,25 @@
+import { mainLogo,mainImage } from "../../shared/assets";
 import { Header } from "../../shared/components";
+import { HomePageContainer, Main } from "./styles";
 
 export function HomePage () {
     return (
-        <>
+        <HomePageContainer>
         <Header/>
-        </>
+
+        <Main>
+            <div className="content-logo">
+                <img src={mainLogo} alt="" />
+            </div>
+            <div className="main-image-container">
+                <img src={mainImage} alt="" />
+            </div>
+            <div>
+                <h2>
+                Móveis planejados para a sua cozinha, sala e quartros
+                </h2>
+            </div>
+        </Main>
+        </HomePageContainer>
     )
 }
